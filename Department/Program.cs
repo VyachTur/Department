a42fb23fe815eff209943cf5460e6d4e2c57d243
Tamplier <tamplier29@mail.ru>
2020-09-18 13:07:49 +1000

    Begin

--- Department/Program.cs
+++ Department/Program.cs
@@ -5,16 +5,7 @@ namespace Department {
         static void Main(string[] args) {
             Employee emp = new Employee("васиЛий", "пУпкИн", "петровиЧ", DateTime.Parse("29.12.1986"));
 
-            //emp.Name = "вася";
-            //emp.Family = "пупкин";
-            //emp.Sirname = "петрович";
-            //emp.BirthDate = new DateTime(1986, 12, 30);
-
-
             Console.WriteLine(emp.returnEmployeeInfo());
-
-
-
         }
     }
 }
